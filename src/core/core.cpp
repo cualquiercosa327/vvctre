c22596d4cad873ccd16fda3a68cdabd0e08cf8b6
James Rowe <jroweboy@gmail.com>
2019-09-09 19:48:40 -0600

    Add a error log for unsupported configurations for disk cache

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -197,8 +197,8 @@ System::ResultStatus System::Init(Frontend::EmuWindow& emu_window, u32 system_mo
 
     timing = std::make_unique<Timing>(Settings::values.cpu_clock_percentage);
 
-    kernel = std::make_unique<Kernel::KernelSystem>(
-        *memory, *timing, [this] { PrepareReschedule(); }, system_mode);
+    kernel = std::make_unique<Kernel::KernelSystem>(*memory, *timing,
+                                                    [this] { PrepareReschedule(); }, system_mode);
 
     if (Settings::values.use_cpu_jit) {
 #ifdef ARCHITECTURE_x86_64
