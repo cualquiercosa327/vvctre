db2e5976e7801e73a84746526f86e0e667e805f1
James Rowe <jroweboy@gmail.com>
2019-12-15 22:04:33 -0700

    Add CPU Clock Frequency slider
    This slider affects the number of cycles that the guest cpu emulation
    reports that have passed since the last time slice. This option scales
    the result returned by a percentage that the user selects. In some games
    underclocking the CPU can give a major speedup. Exposing this as an
    option will give users something to toy with for performance, while also
    potentially enhancing games that experience lag on the real console

--- src/core/core_timing.cpp
+++ src/core/core_timing.cpp
@@ -8,6 +8,7 @@
 #include "common/assert.h"
 #include "common/logging/log.h"
 #include "core/core_timing.h"
+#include "core/settings.h"
 
 namespace Core {
 
@@ -47,7 +48,7 @@ u64 Timing::GetTicks() const {
 }
 
 void Timing::AddTicks(u64 ticks) {
-    downcount -= ticks;
+    downcount -= static_cast<u64>(ticks * 100.0 / Settings ::values.cpu_clock_percentage);
 }
 
 u64 Timing::GetIdleTicks() const {
