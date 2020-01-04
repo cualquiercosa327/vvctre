30966a1b49895677ecd58f6020c11e4ae062531e
vvanelslande <vvanelslandedev@gmail.com>
2020-01-04 18:29:04 -0500

    fix

--- src/vvctre/vvctre.cpp
+++ src/vvctre/vvctre.cpp
@@ -252,30 +252,52 @@ int main(int argc, char** argv) {
     }
     case Command::Poll: {
         InputCommon::Init();
-        std::vector<std::unique_ptr<InputCommon::Polling::DevicePoller>> button_pollers =
-            InputCommon::Polling::GetPollers(InputCommon::Polling::DeviceType::Button);
-        std::vector<std::unique_ptr<InputCommon::Polling::DevicePoller>> analog_pollers =
-            InputCommon::Polling::GetPollers(InputCommon::Polling::DeviceType::Analog);
-        for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : button_pollers) {
-            poller->Start();
-        }
-        for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : analog_pollers) {
-            poller->Start();
-        }
-        for (;;) {
-            for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : button_pollers) {
-                const Common::ParamPackage params = poller->GetNextInput();
-                if (params.Has("engine")) {
-                    std::cout << "Button: " << params.Serialize() << std::endl;
-                }
+        using namespace std::chrono_literals;
+        std::thread([] {
+            std::vector<std::unique_ptr<InputCommon::Polling::DevicePoller>> pollers =
+                InputCommon::Polling::GetPollers(InputCommon::Polling::DeviceType::Button);
+
+            for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : pollers) {
+                poller->Start();
             }
-            for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : analog_pollers) {
-                const Common::ParamPackage params = poller->GetNextInput();
-                if (params.Has("engine")) {
-                    std::cout << "Analog: " << params.Serialize() << std::endl;
+
+            for (;;) {
+                for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : pollers) {
+                    const Common::ParamPackage params = poller->GetNextInput();
+                    if (params.Has("engine")) {
+                        std::cout << "Button: " << params.Serialize() << std::endl;
+                    }
                 }
+
+                std::this_thread::sleep_for(250ms);
             }
-        }
+        })
+            .detach();
+
+        std::thread([] {
+            std::vector<std::unique_ptr<InputCommon::Polling::DevicePoller>> pollers =
+                InputCommon::Polling::GetPollers(InputCommon::Polling::DeviceType::Analog);
+
+            for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : pollers) {
+                poller->Start();
+            }
+
+            for (;;) {
+                for (std::unique_ptr<InputCommon::Polling::DevicePoller>& poller : pollers) {
+                    const Common::ParamPackage params = poller->GetNextInput();
+                    if (params.Has("engine")) {
+                        std::cout << "Analog: " << params.Serialize() << std::endl;
+                    }
+                }
+
+                std::this_thread::sleep_for(250ms);
+            }
+        })
+            .detach();
+
+        std::cout << "Press enter to exit." << std::endl;
+        std::cin.get();
+
         break;
     }
     case Command::Version: {
