18a9216381b2e587f17e1682b19d8a44cbdcd25a
vvanelslande <vvanelslandedev@gmail.com>
2020-01-05 11:59:37 -0500

    tests: set use_custom_cpu_ticks to false

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -2,8 +2,11 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
+#include "core/settings.h"
 
-// Catch provides the main function since we've given it the
-// CATCH_CONFIG_MAIN preprocessor directive.
+int main(int argc, char* argv[]) {
+    Settings::values.use_custom_cpu_ticks = false;
+    return Catch::Session().run(argc, argv);
+}
