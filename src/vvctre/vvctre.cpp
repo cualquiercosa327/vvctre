cba7ad6bd7dd2f50f4e1d4d627690da96128c05c
vvanelslande <vvanelslandedev@gmail.com>
2020-01-23 08:11:56 -0500

    Fixed title bar not updating if disk shader cache is disabled

--- src/vvctre/vvctre.cpp
+++ src/vvctre/vvctre.cpp
@@ -561,14 +561,18 @@ int main(int argc, char** argv) {
 
             std::thread render_thread([&emu_window] { emu_window->Present(); });
 
-            std::atomic_bool stop_run;
-            Core::System::GetInstance().Renderer().Rasterizer()->LoadDiskResources(
-                stop_run,
-                [&](VideoCore::LoadCallbackStage stage, std::size_t value, std::size_t total) {
-                    LOG_DEBUG(Frontend, "Loading stage {} progress {} {}", static_cast<u32>(stage),
-                              value, total);
-                    emu_window->DiskShaderCacheProgress(stage, value, total);
-                });
+            if (Settings::values.use_disk_shader_cache) {
+                std::atomic_bool stop_run;
+                Core::System::GetInstance().Renderer().Rasterizer()->LoadDiskResources(
+                    stop_run,
+                    [&](VideoCore::LoadCallbackStage stage, std::size_t value, std::size_t total) {
+                        LOG_DEBUG(Frontend, "Loading stage {} progress {} {}",
+                                  static_cast<u32>(stage), value, total);
+                        emu_window->DiskShaderCacheProgress(stage, value, total);
+                    });
+            } else {
+                emu_window->DiskShaderCacheProgress(VideoCore::LoadCallbackStage::Complete, 0, 0);
+            }
 
             while (emu_window->IsOpen()) {
                 switch (system.RunLoop()) {
