39353fb3940cc446ff70d863ccd036cf61d40164
vvanelslande <vvanelslandedev@gmail.com>
2020-02-22 00:16:19 -0500

    .

--- src/core/settings.cpp
+++ src/core/settings.cpp
@@ -42,8 +42,6 @@ void Apply() {
 
     auto& system = Core::System::GetInstance();
     if (system.IsPoweredOn()) {
-        system.CoreTiming().UpdateClockSpeed(values.cpu_clock_percentage);
-
         AudioCore::DspInterface& dsp = system.DSP();
         dsp.SetSink(values.sink_id, values.audio_device_id);
         dsp.EnableStretching(values.enable_audio_stretching);
