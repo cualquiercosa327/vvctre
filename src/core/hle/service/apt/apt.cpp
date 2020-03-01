104ccc75cd89412fe6cf6b2f8f2d1ad8add8de30
vvanelslande <vvanelslandedev@gmail.com>
2020-03-01 18:50:53 -0500

    try to fix Windows builds

--- src/core/hle/service/apt/apt.cpp
+++ src/core/hle/service/apt/apt.cpp
@@ -2,7 +2,6 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include <portable-file-dialogs.h>
 #include "common/common_paths.h"
 #include "common/file_util.h"
 #include "common/logging/log.h"
@@ -26,6 +25,8 @@
 #include "core/hle/service/service.h"
 #include "core/hw/aes/ccm.h"
 #include "core/hw/aes/key.h"
+
+#include <portable-file-dialogs.h>
 
 namespace Service::APT {
 
