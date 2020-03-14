65f96132652b1c05fb0c3450fe3f19ce98e2f1be
vvanelslande <vvanelslandedev@gmail.com>
2020-03-14 15:50:46 -0500

    nothing

--- src/video_core/renderer_opengl/texture_filters/bicubic/bicubic.h
+++ src/video_core/renderer_opengl/texture_filters/bicubic/bicubic.h
@@ -9,16 +9,17 @@
 #include "video_core/renderer_opengl/texture_filters/texture_filter_interface.h"
 
 namespace OpenGL {
+
 class Bicubic : public TextureFilterInterface {
 public:
+    Bicubic(u16 scale_factor);
+
     static TextureFilterInfo GetInfo() {
         TextureFilterInfo info;
         info.name = "Bicubic";
         info.constructor = std::make_unique<Bicubic, u16>;
         return info;
     }
-
-    Bicubic(u16 scale_factor);
 
     void scale(CachedSurface& surface, const Common::Rectangle<u32>& rect,
                std::size_t buffer_offset) override;
@@ -30,4 +31,5 @@ private:
     OGLFramebuffer draw_fbo{};
     OGLSampler src_sampler{};
 };
+
 } // namespace OpenGL
