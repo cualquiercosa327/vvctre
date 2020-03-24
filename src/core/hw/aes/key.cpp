92270624013bff7f94b4c4d7c9a8fa0d6c80a2dc
vvanelslande <vvanelslandedev@gmail.com>
2020-03-24 12:41:08 -0500

    Remove aes_keys.txt support, fixes fatal error in Tomodachi Life.

--- src/core/hw/aes/key.cpp
+++ src/core/hw/aes/key.cpp
@@ -40,19 +40,6 @@ struct KeyDesc {
     bool same_as_before;
 };
 
-AESKey HexToKey(const std::string& hex) {
-    if (hex.size() < 32) {
-        throw std::invalid_argument("hex string is too short");
-    }
-
-    AESKey key;
-    for (std::size_t i = 0; i < key.size(); ++i) {
-        key[i] = static_cast<u8>(std::stoi(hex.substr(i * 2, 2), 0, 16));
-    }
-
-    return key;
-}
-
 struct KeySlot {
     std::optional<AESKey> x;
     std::optional<AESKey> y;
@@ -425,86 +412,19 @@ void LoadNativeFirmKeysNew3DS() {
     }
 }
 
-void LoadPresetKeys() {
-    const std::string filepath = FileUtil::GetUserPath(FileUtil::UserPath::SysDataDir) + AES_KEYS;
-    FileUtil::CreateFullPath(filepath); // Create path if not already created
-    std::ifstream file;
-    OpenFStream(file, filepath, std::ios_base::in);
-    if (!file) {
-        return;
-    }
-
-    while (!file.eof()) {
-        std::string line;
-        std::getline(file, line);
-        std::vector<std::string> parts;
-        Common::SplitString(line, '=', parts);
-        if (parts.size() != 2) {
-            LOG_ERROR(HW_AES, "Failed to parse {}", line);
-            continue;
-        }
-
-        const std::string& name = parts[0];
-        AESKey key;
-        try {
-            key = HexToKey(parts[1]);
-        } catch (const std::logic_error& e) {
-            LOG_ERROR(HW_AES, "Invalid key {}: {}", parts[1], e.what());
-            continue;
-        }
-
-        std::size_t common_key_index;
-        if (std::sscanf(name.c_str(), "common%zd", &common_key_index) == 1) {
-            if (common_key_index >= common_key_y_slots.size()) {
-                LOG_ERROR(HW_AES, "Invalid common key index {}", common_key_index);
-            } else {
-                common_key_y_slots[common_key_index] = key;
-            }
-            continue;
-        }
-
-        std::size_t slot_id;
-        char key_type;
-        if (std::sscanf(name.c_str(), "slot0x%zXKey%c", &slot_id, &key_type) != 2) {
-            LOG_ERROR(HW_AES, "Invalid key name {}", name);
-            continue;
-        }
-
-        if (slot_id >= MaxKeySlotID) {
-            LOG_ERROR(HW_AES, "Out of range slot ID {:#X}", slot_id);
-            continue;
-        }
-
-        switch (key_type) {
-        case 'X':
-            key_slots.at(slot_id).SetKeyX(key);
-            break;
-        case 'Y':
-            key_slots.at(slot_id).SetKeyY(key);
-            break;
-        case 'N':
-            key_slots.at(slot_id).SetNormalKey(key);
-            break;
-        default:
-            LOG_ERROR(HW_AES, "Invalid key type {}", key_type);
-            break;
-        }
-    }
-}
-
 } // namespace
 
 void InitKeys() {
     static bool initialized = false;
-    if (initialized)
+    if (initialized) {
         return;
+    }
     initialized = true;
     HW::RSA::InitSlots();
     LoadBootromKeys();
     LoadNativeFirmKeysOld3DS();
     LoadSafeModeNativeFirmKeysOld3DS();
     LoadNativeFirmKeysNew3DS();
-    LoadPresetKeys();
 }
 
 void SetKeyX(std::size_t slot_id, const AESKey& key) {
