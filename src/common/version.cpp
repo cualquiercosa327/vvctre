034b1a31c6f9e9855f875369ad45c4c394336bfd
vvanelslande <vvanelslandedev@gmail.com>
2020-03-24 11:58:39 -0500

    Update version

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -5,7 +5,7 @@
 #include "common/version.h"
 
 namespace version {
-const semver::version vvctre{15, 5, 1};
+const semver::version vvctre{15, 6, 0};
 const u8 movie = 1;
 const u8 shader_cache = 2;
 } // namespace version
