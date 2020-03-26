782006171de33b28b513cedb4776c653e16f1504
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-03-25 19:12:35 -0600

    Update Discord invite

--- src/vvctre/emu_window/emu_window_sdl2.cpp
+++ src/vvctre/emu_window/emu_window_sdl2.cpp
@@ -1384,9 +1384,9 @@ void EmuWindow_SDL2::SwapBuffers() {
             if (ImGui::BeginMenu("Help")) {
                 if (ImGui::MenuItem("Discord Server")) {
 #ifdef _WIN32
-                    const int code = std::system("start https://discord.gg/RNBCBzT");
+                    const int code = std::system("start https://discord.gg/fUrNqwA");
 #else
-                    const int code = std::system("xdg-open https://discord.gg/RNBCBzT");
+                    const int code = std::system("xdg-open https://discord.gg/fUrNqwA");
 #endif
                     LOG_INFO(Frontend, "Opened Discord invite, exit code: {}", code);
                 }
