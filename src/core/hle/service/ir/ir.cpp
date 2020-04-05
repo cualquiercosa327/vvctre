6bf4dfc62debbbad808254b95a998e07b46ea630
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-04-05 16:52:25 -0500

    fix

--- src/core/hle/service/ir/ir.cpp
+++ src/core/hle/service/ir/ir.cpp
@@ -5,7 +5,6 @@
 #include <memory>
 #include "core/core.h"
 #include "core/hle/service/ir/ir.h"
-#include "core/hle/service/ir/ir_rst.h"
 #include "core/hle/service/ir/ir_u.h"
 #include "core/hle/service/ir/ir_user.h"
 #include "core/hle/service/service.h"
