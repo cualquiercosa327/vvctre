dd81d290668493c2d55a8ec603d86d4626a1adac
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-04-06 12:50:35 -0500

    add include

--- src/core/rpc/server.cpp
+++ src/core/rpc/server.cpp
@@ -2,6 +2,7 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <sstream>
 #include <httplib.h>
 #include <json.hpp>
 #include "common/logging/backend.h"
