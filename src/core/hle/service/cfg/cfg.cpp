69b11ba0e93896c4aa4bcd07117a24bf6433df5b
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-04-11 18:41:58 -0500

    change default system settings

--- src/core/hle/service/cfg/cfg.cpp
+++ src/core/hle/service/cfg/cfg.cpp
@@ -93,11 +93,11 @@ static const EULAVersion MAX_EULA_VERSION = {0x7F, 0x7F};
 static const ConsoleModelInfo CONSOLE_MODEL = {NINTENDO_3DS, {0, 0, 0}};
 static const u8 CONSOLE_LANGUAGE = LANGUAGE_EN;
 static const UsernameBlock CONSOLE_USERNAME_BLOCK = {u"vvctre", 0, 0};
-static const BirthdayBlock PROFILE_BIRTHDAY = {3, 25}; // March 25th, 2014
-static const u8 SOUND_OUTPUT_MODE = SOUND_SURROUND;
-static const u8 UNITED_STATES_COUNTRY_ID = 49;
+static const BirthdayBlock PROFILE_BIRTHDAY = {1, 5}; // March 25th, 2014
+static const u8 SOUND_OUTPUT_MODE = SOUND_STEREO;
+
 /// TODO(Subv): Find what the other bytes are
-static const ConsoleCountryInfo COUNTRY_INFO = {{0, 0, 0}, UNITED_STATES_COUNTRY_ID};
+static const ConsoleCountryInfo COUNTRY_INFO = {{0, 0, 0}, 36};
 
 /**
  * TODO(Subv): Find out what this actually is, these values fix some NaN uniforms in some games,
