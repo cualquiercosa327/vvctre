1ad096cc8bf1aa3f19160d9c59472f7f6e693727
B3n30 <benediktthomas@gmail.com>
2020-05-01 15:36:09 +0200

    Port https://github.com/citra-emu/citra/pull/5305

--- src/core/cheats/cheats.cpp
+++ src/core/cheats/cheats.cpp
@@ -15,7 +15,9 @@
 
 namespace Cheats {
 
-constexpr u64 run_interval_ticks = BASE_CLOCK_RATE_ARM11 / GPU::SCREEN_REFRESH_RATE;
+// Luma3DS uses this interval for applying cheats, so to keep consistent behavior
+// we use the same value
+constexpr u64 run_interval_ticks = 50'000'000;
 
 CheatEngine::CheatEngine(Core::System& system_) : system(system_) {
     LoadCheatFile();
