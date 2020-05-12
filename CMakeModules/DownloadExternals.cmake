bb7d2fcfeff7d5d71c2eeb0862b0c24ba45154c2
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-05-11 19:21:38 -0500

    Indent some code

--- CMakeModules/DownloadExternals.cmake
+++ CMakeModules/DownloadExternals.cmake
@@ -3,15 +3,15 @@
 #   remote_path: path to the file to download, relative to the remote repository root
 #   prefix_var: name of a variable which will be set with the path to the extracted contents
 function(download_bundled_external remote_path lib_name prefix_var)
-set(prefix "${PROJECT_BINARY_DIR}/externals/${lib_name}")
-if (NOT EXISTS "${prefix}")
-    message(STATUS "Downloading binaries for ${lib_name}...")
-    file(DOWNLOAD
-        https://github.com/vvanelslande/vvctre-windows-dependency-binaries/raw/master/${remote_path}${lib_name}.7z
-        "${PROJECT_BINARY_DIR}/externals/${lib_name}.7z" SHOW_PROGRESS)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf "${PROJECT_BINARY_DIR}/externals/${lib_name}.7z"
-        WORKING_DIRECTORY "${PROJECT_BINARY_DIR}/externals")
-endif()
-message(STATUS "Using bundled binaries at ${prefix}")
-set(${prefix_var} "${prefix}" PARENT_SCOPE)
+    set(prefix "${PROJECT_BINARY_DIR}/externals/${lib_name}")
+    if (NOT EXISTS "${prefix}")
+        message(STATUS "Downloading binaries for ${lib_name}...")
+        file(DOWNLOAD
+            https://github.com/vvanelslande/vvctre-windows-dependency-binaries/raw/master/${remote_path}${lib_name}.7z
+            "${PROJECT_BINARY_DIR}/externals/${lib_name}.7z" SHOW_PROGRESS)
+        execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf "${PROJECT_BINARY_DIR}/externals/${lib_name}.7z"
+            WORKING_DIRECTORY "${PROJECT_BINARY_DIR}/externals")
+    endif()
+    message(STATUS "Using bundled binaries at ${prefix}")
+    set(${prefix_var} "${prefix}" PARENT_SCOPE)
 endfunction()
