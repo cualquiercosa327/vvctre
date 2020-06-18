cualquiercosa327/vvctre

commit 8fb86097885a0a787e511fbdbdea66bc090c27be

remove nlohmann/json, use asl

src/vvctre/common.cpp
@@ -5,7 +5,6 @@
 
 #include <asl/Http.h>
 #include <fmt/format.h>
-#include <nlohmann/json.hpp>
 #include "common/file_util.h"
 #include "common/logging/log.h"
 #include "core/hle/service/am/am.h"
@@ -17,31 +16,6 @@
 const u8 vvctre_version_major = 34;
 const u8 vvctre_version_minor = 1;
 const u8 vvctre_version_patch = 0;
-
-void from_json(const nlohmann::json& json, CitraRoom::Member& member) {
-    // nothing to do
-}
-
-void from_json(const nlohmann::json& json, CitraRoom& room) {
-    room.ip = json.at("address").get<std::string>();
-    room.name = json.at("name").get<std::string>();
-    try {
-        room.description = json.at("description").get<std::string>();
-    } catch (const nlohmann::detail::out_of_range& e) {
-        room.description = "";
-        LOG_DEBUG(Network, "Room \'{}\' doesn't contain a description", room.name);
-    }
-    room.owner = json.at("owner").get<std::string>();
-    room.port = json.at("port").get<u16>();
-    room.max_players = json.at("maxPlayers").get<u32>();
-    room.net_version = json.at("netVersion").get<u32>();
-    room.has_password = json.at("hasPassword").get<bool>();
-    try {
-        room.members = json.at("players").get<std::vector<CitraRoom::Member>>();
-    } catch (const nlohmann::detail::out_of_range& e) {
-        LOG_DEBUG(Network, "Out of range {}", e.what());
-    }
-}
 
 std::vector<std::tuple<std::string, std::string>> GetInstalledList() {
     std::vector<std::tuple<std::string, std::string>> all;
@@ -124,7 +98,26 @@ CitraRoomList GetPublicCitraRooms() {
         return {};
     }
 
-    return nlohmann::json::parse(std::string(static_cast<const char*>(r.text())))
-        .at("rooms")
-        .get<CitraRoomList>();
+    asl::Var json_rooms = r.json()("rooms");
+
+    CitraRoomList rooms;
+
+    for (int i = 0; i < json_rooms.length(); ++i) {
+        asl::Var json_room = json_rooms[i];
+
+        CitraRoom room;
+        room.name = *json_room("name");
+        if (json_room.has("description")) {
+            room.description = *json_room("description");
+        }
+        room.ip = *json_room("ip");
+        room.port = static_cast<u16>(static_cast<int>(json_room("port")));
+        room.owner = *json_room("owner");
+        room.max_players = json_room("maxPlayers");
+        room.has_password = json_room("hasPassword");
+        room.members.resize(json_room("members").length());
+        rooms.push_back(std::move(room));
+    }
+
+    return rooms;
 }

.gitmodules
@@ -40,9 +40,6 @@
 [submodule "portable-file-dialogs"]
     path = externals/portable-file-dialogs
     url = https://github.com/samhocevar/portable-file-dialogs.git
-[submodule "json"]
-    path = externals/json
-    url = https://github.com/nlohmann/json.git
 [submodule "stb"]
     path = externals/stb
     url = https://github.com/nothings/stb.git

externals/CMakeLists.txt
@@ -97,11 +97,6 @@ add_library(stb
 )
 target_include_directories(stb PUBLIC ./stb)
 
-# json
-set(JSON_BuildTests OFF CACHE BOOL "")
-set(JSON_Install OFF CACHE BOOL "")
-add_subdirectory(json EXCLUDE_FROM_ALL)
-
 # mbedTLS
 set(ENABLE_PROGRAMS OFF CACHE BOOL "" FORCE)
 set(ENABLE_TESTING OFF CACHE BOOL "" FORCE)

src/vvctre/CMakeLists.txt
@@ -27,7 +27,7 @@ add_executable(vvctre
 create_target_directory_groups(vvctre)
 
 target_link_libraries(vvctre PRIVATE common core input_common audio_core network)
-target_link_libraries(vvctre PRIVATE stb opengl-loader clip portable_file_dialogs imgui nlohmann_json asl ${PLATFORM_LIBRARIES} Threads::Threads)
+target_link_libraries(vvctre PRIVATE stb opengl-loader clip portable_file_dialogs imgui asl ${PLATFORM_LIBRARIES} Threads::Threads)
 
 if (MSVC)
     include(CopySDLDeps)

src/vvctre/common.h
@@ -15,19 +15,15 @@ extern const u8 vvctre_version_minor;
 extern const u8 vvctre_version_patch;
 
 struct CitraRoom {
-    struct Member {
-        // nothing is used
-    };
-
     std::string name;
     std::string description;
     std::string owner;
     std::string ip;
     u16 port;
     u32 max_players;
-    u32 net_version;
     bool has_password;
 
+    struct Member {};
     std::vector<Member> members;
 };
 

src/vvctre/initial_settings.cpp
@@ -16,7 +16,7 @@
 #ifdef HAVE_CUBEB
 #include "audio_core/cubeb_input.h"
 #endif
-#include <nlohmann/json.hpp>
+#include <asl/JSON.h>
 #include "audio_core/sink.h"
 #include "audio_core/sink_details.h"
 #include "common/file_util.h"
@@ -1761,32 +1761,22 @@ InitialSettings::InitialSettings(PluginManager& plugin_manager, SDL_Window* wind
                         const std::vector<std::string> path =
                             pfd::open_file("Load File", ".", {"JSON Files", "*.json"}).result();
                         if (!path.empty()) {
-                            std::string contents;
-                            FileUtil::ReadFileToString(true, path[0], contents);
-
-                            try {
-                                const nlohmann::json json = nlohmann::json::parse(contents);
-                                Settings::values.buttons =
-                                    json["buttons"]
-                                        .get<std::array<std::string,
-                                                        Settings::NativeButton::NumButtons>>();
-                                Settings::values.analogs =
-                                    json["analogs"]
-                                        .get<std::array<std::string,
-                                                        Settings::NativeAnalog::NumAnalogs>>();
-                                Settings::values.motion_device =
-                                    json["motion_device"].get<std::string>();
-                                Settings::values.touch_device =
-                                    json["touch_device"].get<std::string>();
-                                Settings::values.cemuhookudp_address =
-                                    json["cemuhookudp_address"].get<std::string>();
-                                Settings::values.cemuhookudp_port =
-                                    json["cemuhookudp_port"].get<u16>();
-                                Settings::values.cemuhookudp_pad_index =
-                                    json["cemuhookudp_pad_index"].get<u8>();
-                            } catch (nlohmann::json::exception& exception) {
-                                pfd::message("JSON Exception", exception.what(), pfd::choice::ok);
+                            asl::Var json = asl::Json::read(path[0].c_str());
+                            asl::Array buttons = json["buttons"].array();
+                            asl::Array analogs = json["analogs"].array();
+                            for (int i = 0; i < buttons.length(); ++i) {
+                                Settings::values.buttons[static_cast<std::size_t>(i)] = *buttons[i];
                             }
+                            for (int i = 0; i < analogs.length(); ++i) {
+                                Settings::values.analogs[static_cast<std::size_t>(i)] = *analogs[i];
+                            }
+                            Settings::values.motion_device = *json["motion_device"];
+                            Settings::values.touch_device = *json["touch_device"];
+                            Settings::values.cemuhookudp_address = *json["cemuhookudp_address"];
+                            Settings::values.cemuhookudp_port =
+                                static_cast<u16>(static_cast<int>(json["cemuhookudp_port"]));
+                            Settings::values.cemuhookudp_pad_index =
+                                static_cast<u8>(static_cast<int>(json["cemuhookudp_pad_index"]));
                         }
                     }
                     ImGui::SameLine();
@@ -1796,20 +1786,28 @@ InitialSettings::InitialSettings(PluginManager& plugin_manager, SDL_Window* wind
                                 .result();
 
                         if (!path.empty()) {
-                            const std::string json =
-                                nlohmann::json{
-                                    {"buttons", Settings::values.buttons},
-                                    {"analogs", Settings::values.analogs},
-                                    {"motion_device", Settings::values.motion_device},
-                                    {"touch_device", Settings::values.touch_device},
-                                    {"cemuhookudp_address", Settings::values.cemuhookudp_address},
-                                    {"cemuhookudp_port", Settings::values.cemuhookudp_port},
-                                    {"cemuhookudp_pad_index",
-                                     Settings::values.cemuhookudp_pad_index},
-                                }
-                                    .dump();
+                            asl::Array<asl::String> buttons;
+                            asl::Array<asl::String> analogs;
+                            for (const std::string& button : Settings::values.buttons) {
+                                buttons << asl::String(button.c_str());
+                            }
+                            for (const std::string& analog : Settings::values.analogs) {
+                                analogs << asl::String(analog.c_str());
+                            }
 
-                            FileUtil::WriteStringToFile(true, path, json);
+                            asl::Var json;
+                            json["buttons"] = buttons;
+                            json["analogs"] = analogs;
+                            json["motion_device"] = Settings::values.motion_device.c_str();
+                            json["touch_device"] = Settings::values.touch_device.c_str();
+                            json["cemuhookudp_address"] =
+                                Settings::values.cemuhookudp_address.c_str();
+                            json["cemuhookudp_port"] =
+                                static_cast<int>(Settings::values.cemuhookudp_port);
+                            json["cemuhookudp_pad_index"] =
+                                static_cast<int>(Settings::values.cemuhookudp_pad_index);
+
+                            asl::Json::write(path.c_str(), json, false);
                         }
                     }
                     ImGui::NewLine();

src/vvctre/plugins.cpp
@@ -6,9 +6,9 @@
 #include <utility>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#include <asl/JSON.h>
 #include <fmt/format.h>
 #include <imgui.h>
-#include <nlohmann/json.hpp>
 #include "common/common_funcs.h"
 #include "common/file_util.h"
 #include "common/logging/log.h"
@@ -46,6 +46,15 @@
 bool has_suffix(const std::string& str, const std::string& suffix) {
     return str.size() >= suffix.size() &&
            str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+template <typename T>
+asl::Array<T> STD_VECTOR_TO_ASL_ARRAY(const std::vector<T>& source) {
+    asl::Array<T> destination;
+    for (const T& x : source) {
+        destination << x;
+    }
+    return destination;
 }
 
 PluginManager::PluginManager(Core::System& core, SDL_Window* window) : window(window) {
@@ -424,37 +433,46 @@ bool vvctre_ipc_recorder_get_enabled(void* core) {
 void vvctre_ipc_recorder_bind_callback(void* core, void (*callback)(const char* json)) {
     static_cast<Core::System*>(core)->Kernel().GetIPCRecorder().BindCallback(
         [callback](const IPCDebugger::RequestRecord& record) {
-            nlohmann::json json;
+            asl::Var json;
             json["id"] = record.id;
             json["status"] = static_cast<int>(record.status);
-            json["client_process"]["type"] = record.client_process.type;
-            json["client_process"]["name"] = record.client_process.name;
-            json["client_process"]["id"] = record.client_process.id;
-            json["client_thread"]["type"] = record.client_thread.type;
-            json["client_thread"]["name"] = record.client_thread.name;
-            json["client_thread"]["id"] = record.client_thread.id;
-            json["client_session"]["type"] = record.client_session.type;
-            json["client_session"]["name"] = record.client_session.name;
-            json["client_session"]["id"] = record.client_session.id;
-            json["client_port"]["type"] = record.client_port.type;
-            json["client_port"]["name"] = record.client_port.name;
-            json["client_port"]["id"] = record.client_port.id;
-            json["server_process"]["type"] = record.server_process.type;
-            json["server_process"]["name"] = record.server_process.name;
-            json["server_process"]["id"] = record.server_process.id;
-            json["server_thread"]["type"] = record.server_thread.type;
-            json["server_thread"]["name"] = record.server_thread.name;
-            json["server_thread"]["id"] = record.server_thread.id;
-            json["server_session"]["type"] = record.server_session.type;
-            json["server_session"]["name"] = record.server_session.name;
-            json["server_session"]["id"] = record.server_session.id;
-            json["function_name"] = record.function_name;
+            json["function_name"] = record.function_name.c_str();
             json["is_hle"] = record.is_hle;
-            json["untranslated_request_cmdbuf"] = record.untranslated_request_cmdbuf;
-            json["translated_request_cmdbuf"] = record.translated_request_cmdbuf;
-            json["untranslated_reply_cmdbuf"] = record.untranslated_reply_cmdbuf;
-            json["translated_reply_cmdbuf"] = record.translated_reply_cmdbuf;
-            callback(json.dump().c_str());
+            json["client_process"]["type"] = record.client_process.type.c_str();
+            json["client_process"]["name"] = record.client_process.name.c_str();
+            json["client_process"]["id"] = record.client_process.id;
+            json["client_thread"]["type"] = record.client_thread.type.c_str();
+            json["client_thread"]["name"] = record.client_thread.name.c_str();
+            json["client_thread"]["id"] = record.client_thread.id;
+            json["client_session"]["type"] = record.client_session.type.c_str();
+            json["client_session"]["name"] = record.client_session.name.c_str();
+            json["client_session"]["id"] = record.client_session.id;
+            json["client_port"]["type"] = record.client_port.type.c_str();
+            json["client_port"]["name"] = record.client_port.name.c_str();
+            json["client_port"]["id"] = record.client_port.id;
+            json["server_process"]["type"] = record.server_process.type.c_str();
+            json["server_process"]["name"] = record.server_process.name.c_str();
+            json["server_process"]["id"] = record.server_process.id;
+            json["server_thread"]["type"] = record.server_thread.type.c_str();
+            json["server_thread"]["name"] = record.server_thread.name.c_str();
+            json["server_thread"]["id"] = record.server_thread.id;
+            json["server_session"]["type"] = record.server_session.type.c_str();
+            json["server_session"]["name"] = record.server_session.name.c_str();
+            json["server_session"]["id"] = record.server_session.id;
+            for (std::size_t i = 0; i < record.untranslated_request_cmdbuf.size(); ++i) {
+                json["untranslated_request_buf"][i] = record.untranslated_request_cmdbuf[i];
+            }
+            for (std::size_t i = 0; i < record.translated_request_cmdbuf.size(); ++i) {
+                json["translated_request_cmdbuf"][i] = record.translated_request_cmdbuf[i];
+            }
+            for (std::size_t i = 0; i < record.untranslated_reply_cmdbuf.size(); ++i) {
+                json["untranslated_reply_cmdbuf"][i] = record.untranslated_reply_cmdbuf[i];
+            }
+            for (std::size_t i = 0; i < record.translated_reply_cmdbuf.size(); ++i) {
+                json["translated_reply_cmdbuf"][i] = record.translated_reply_cmdbuf[i];
+            }
+
+            callback(*asl::Json::encode(json));
         });
 }
 

commit 6f8e8c7f8c6c772fcbd598e0e582a42baeaf748b

fix another public rooms issue (caused by 8fb86097885a0a787e511fbdbdea66bc090c27be)

src/vvctre/common.cpp
@@ -110,7 +110,7 @@ CitraRoomList GetPublicCitraRooms() {
         if (json_room.has("description")) {
             room.description = *json_room("description");
         }
-        room.ip = *json_room("ip");
+        room.ip = *json_room("address");
         room.port = static_cast<u16>(static_cast<int>(json_room("port")));
         room.owner = *json_room("owner");
         room.max_players = json_room("maxPlayers");