a89f9221f396f5931ae586438160d00a9ebab611
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-06-18 07:57:29 -0500

    remove unused include

--- src/vvctre/vvctre.cpp
+++ src/vvctre/vvctre.cpp
@@ -23,7 +23,6 @@
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_sdl.h>
 #include <portable-file-dialogs.h>
-#include "common/file_util.h"
 #include "common/logging/backend.h"
 #include "common/logging/filter.h"
 #include "common/logging/log.h"
