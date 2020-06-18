cualquiercosa327/vvctre

commit dc39424becdabbe9a63c4044c4e13dae3f103263

use asl for public room search

src/vvctre/emu_window/emu_window_sdl2.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#include <asl/String.h>
 #include <clip.h>
 #include <fmt/format.h>
 #include <imgui.h>
@@ -2761,8 +2762,9 @@ void EmuWindow_SDL2::SwapBuffers() {
                 for (const auto& title : installed) {
                     const auto [path, name] = title;
 
-                    if (Common::ToLower(name).find(Common::ToLower(installed_query)) !=
-                            std::string::npos &&
+                    if (asl::String(name.c_str())
+                            .toLowerCase()
+                            .contains(asl::String(installed_query.c_str()).toLowerCase()) &&
                         ImGui::Selectable(name.c_str())) {
                         system.SetResetFilePath(path);
                         system.RequestReset();
@@ -2819,8 +2821,10 @@ void EmuWindow_SDL2::SwapBuffers() {
                         room.has_password ? "{} ({}/{}) by {} (has password)" : "{} ({}/{}) by {}",
                         room.name, room.members.size(), room.max_players, room.owner);
 
-                    if (Common::ToLower(room_string).find(Common::ToLower(public_rooms_query)) !=
-                        std::string::npos) {
+                    if (asl::String(room_string.c_str())
+                            .toLowerCase()
+                            .contains(asl::String(public_rooms_query.c_str()).toLowerCase()) &&
+                        ImGui::Selectable(room_string.c_str())) {
                         if (ImGui::Selectable(room_string.c_str())) {
                             Settings::values.multiplayer_ip = room.ip;
                             Settings::values.multiplayer_port = room.port;

src/vvctre/initial_settings.cpp
@@ -2700,9 +2700,11 @@ InitialSettings::InitialSettings(PluginManager& plugin_manager, SDL_Window* wind
                                                   : "{} ({}/{}) by {}",
                                 room.name, room.members.size(), room.max_players, room.owner);
 
-                            if (Common::ToLower(room_string)
-                                    .find(Common::ToLower(public_rooms_query)) !=
-                                std::string::npos) {
+                            if (asl::String(room_string.c_str())
+                                    .toLowerCase()
+                                    .contains(
+                                        asl::String(public_rooms_query.c_str()).toLowerCase()) &&
+                                ImGui::Selectable(room_string.c_str())) {
                                 if (ImGui::Selectable(room_string.c_str())) {
                                     Settings::values.multiplayer_ip = room.ip;
                                     Settings::values.multiplayer_port = room.port;
@@ -2752,8 +2754,9 @@ InitialSettings::InitialSettings(PluginManager& plugin_manager, SDL_Window* wind
                     for (const auto& title : installed) {
                         const auto [path, name] = title;
 
-                        if (Common::ToLower(name).find(Common::ToLower(installed_query)) !=
-                                std::string::npos &&
+                        if (asl::String(name.c_str())
+                                .toLowerCase()
+                                .contains(asl::String(installed_query.c_str()).toLowerCase()) &&
                             ImGui::Selectable(name.c_str())) {
                             Settings::values.file_path = path;
                             installed.clear();

commit 677231b5e1cbc312fdd66ecc106de1bb953ade7d

fix public rooms (caused by dc39424becdabbe9a63c4044c4e13dae3f103263)

src/vvctre/emu_window/emu_window_sdl2.cpp
@@ -2823,8 +2823,7 @@ void EmuWindow_SDL2::SwapBuffers() {
 
                     if (asl::String(room_string.c_str())
                             .toLowerCase()
-                            .contains(asl::String(public_rooms_query.c_str()).toLowerCase()) &&
-                        ImGui::Selectable(room_string.c_str())) {
+                            .contains(asl::String(public_rooms_query.c_str()).toLowerCase())) {
                         if (ImGui::Selectable(room_string.c_str())) {
                             Settings::values.multiplayer_ip = room.ip;
                             Settings::values.multiplayer_port = room.port;

src/vvctre/initial_settings.cpp
@@ -2703,8 +2703,7 @@ InitialSettings::InitialSettings(PluginManager& plugin_manager, SDL_Window* wind
                             if (asl::String(room_string.c_str())
                                     .toLowerCase()
                                     .contains(
-                                        asl::String(public_rooms_query.c_str()).toLowerCase()) &&
-                                ImGui::Selectable(room_string.c_str())) {
+                                        asl::String(public_rooms_query.c_str()).toLowerCase())) {
                                 if (ImGui::Selectable(room_string.c_str())) {
                                     Settings::values.multiplayer_ip = room.ip;
                                     Settings::values.multiplayer_port = room.port;